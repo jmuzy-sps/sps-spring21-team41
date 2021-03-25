663974981f5e055b2327f6d1fa0564109d81bd37
joshua Hernandez <joshua.hdz.reza@gmail.com>
2021-03-25 22:08:39 +0000

    Location is part of event now

--- src/main/java/com/google/sps/data/Event.java
+++ src/main/java/com/google/sps/data/Event.java
@@ -1,5 +1,7 @@
 package com.google.sps.data;
 
+import com.google.sps.data.Location;
+
 
 /**
  * Event object for easy json packaing.
@@ -9,18 +11,20 @@ public final class Event{
     private String type;
     private String details;
     private float price;
+    private Location address;
 
-    public Event(String type, String details, int price){
+    public Event(String type, String details, int price, Location address) {
         setType(type);
         setDetails(details);
         setPrice(price);
+        setAddress(address);
     }
 
-    public void setType(String type){
+    public void setType(String type) {
         this.type = type;
     }
 
-    public void setDetails(String details){
+    public void setDetails(String details) {
         this.details = details;
     }
 
@@ -28,15 +32,34 @@ public final class Event{
         this.price = price;
     }
 
-    public String getType(){
+    public void setAddress(Location address) {
+        this.address = address;
+    }
+
+    public String getType() {
         return this.type;
     }
 
-    public String getDetails(){
+    public String getDetails() {
         return this.details;
     }
 
-    public float getPrice(){
+    public float getPrice() {
         return this.price;
+    }
+
+    public String getAddress() {
+        return "" + address.getStreet() + ", " + address.getZipCode() + " " + address.getState();
+    }
+
+    public String getAddressStreet(){
+        return address.getStreet();
+    }
+
+    public String getAddressState(){
+        return address.getStreet();
+    }
+    public int getAddressZipCode(){
+        return address.getZipCode();
     }
 }
