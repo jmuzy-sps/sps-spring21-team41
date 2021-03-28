09662ef3699bd43620ddc257e73c537265a926fc
joshua Hernandez <joshua.hdz.reza@gmail.com>
2021-03-28 21:40:43 +0000

    add barebones servlet class

--- src/main/java/com/google/sps/servlets/EventRetrievalServlet.java
+++ src/main/java/com/google/sps/servlets/EventRetrievalServlet.java
@@ -1,1 +1,19 @@
-// This Servlet will retrieve events which will eventually be listed on home page.
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * This Servlet will retrieve events which will eventually be listed on home page.
+ */
+@WebServlet("/")
+public final class EventRetrievalServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    
+  }
+}
