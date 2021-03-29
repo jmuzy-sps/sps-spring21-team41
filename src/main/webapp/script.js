df428dea32f27cfbb196c23db94073e7e2a444c4
carinegordillo <64708532+carinegordillo@users.noreply.github.com>
2021-03-28 23:20:01 -0700

    Delete script.js

--- src/main/webapp/script.js
+++
@@ -1,6 +0,0 @@
-	function addHyphen (element) {
-    	let ele = document.getElementById(element.id);
-        ele = ele.value.split('-').join('');    // Remove dash (-) if mistakenly entered.
-        let finalVal = ele.match(/.{1,3}/g).join('-');
-        document.getElementById("date").value = finalVal;
-    }
